c295836f5b9294bc5316cac1b0b68be24f3e30fe
Day-Mk4 <amadeo.pena@enmu.edu>
2025-10-27 22:48:25 -0600

    Create User.java

---
+++ Stage 3/User.java
@@ -0,0 +1,78 @@
+/**
+ * Author: Amadeo Pena
+ * Assignment: Project
+ */
+public class User {
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+
+    public User(String username, String password, String name,
+                String email, String phone, String address) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getUsername() 
+    { return username; }
+    
+    public void setUsername(String newUsername) 
+    { this.username = newUsername; }
+    
+    public String getPassword() 
+        { return password; 
+    }
+    
+    public void setPassword(String newPassword) 
+        { this.password = newPassword; 
+    }
+    
+    public String getName() 
+        { return name; 
+    }
+    
+    public void setName(String name) 
+        { this.name = name; 
+    }
+    
+    public String getEmail() 
+        { return email; 
+    }
+    
+    public void setEmail(String email) 
+        { this.email = email; 
+    }
+    
+    public String getPhone() 
+        { return phone; 
+    }
+    
+    public void setPhone(String phone) 
+        { this.phone = phone; 
+    }
+    
+    public String getAddress() 
+        { return address; 
+    }
+    
+    public void setAddress(String address) 
+        { this.address = address; 
+    }
+    
+
+    public void displayDetails() {
+    System.out.println("User: " + username);
+    System.out.println("Name: " + name);
+    System.out.println("Email: " + email);
+    System.out.println("Phone: " + phone);
+    System.out.println("Address: " + address);
+}
+
+}
