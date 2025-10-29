3bfdf6ae01d438bce15698f5fe18c53c65b4e81a
Day-Mk4 <amadeo.pena@enmu.edu>
2025-10-28 19:22:37 -0600

    Create Initializer.java

---
+++ Stage 3/Initializer.java
@@ -0,0 +1,10 @@
+/**
+ * Author: Amadeo Pena
+ * Assignment: Project
+ */
+public class Initializer {
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.loginMenu();
+    }
+}
