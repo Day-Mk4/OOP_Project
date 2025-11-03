7e20a4a9a1162b753b993af7fb7bf76854b5b8db
Day-Mk4 <amadeo.pena@enmu.edu>
2025-11-03 10:35:37 -0700

    Create Employee.java

---
+++ Stage 3/Employee.java
@@ -0,0 +1,21 @@
+/**
+ * Author: Amadeo Pena
+ * Assignment: Project
+ */
+public class Employee {
+    public void displayCustomers() 
+        { System.out.println("Display customers"); 
+    }
+    
+    public void displayRestaurants() 
+        { System.out.println("Display restaurants"); 
+    }
+    
+    public void displayDeliveryPersons() 
+        { System.out.println("Display delivery persons"); 
+    }
+    
+    public void displayDeliveryVehicles() 
+        { System.out.println("Display delivery vehicles"); 
+    }
+}
